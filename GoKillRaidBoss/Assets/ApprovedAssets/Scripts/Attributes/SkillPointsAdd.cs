7769a2b417f6a8b9b6c23d64d517c3fbbc5b8f73
Ninbous <Nibous@mail.ru>
2018-01-21 15:38:47 +0300

    error git pull

---
+++ GoKillRaidBoss/Assets/ApprovedAssets/Scripts/Attributes/SkillPointsAdd.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SkillPointsAdd : MonoBehaviour {
+
+    //При клике добавляется очко в аттрибут (прокачка аттрибутов)
+
+    public GameObject player;
+
+    public AudioSource audioSource;
+    public AudioClip[] sounds;
+
+    void Start() {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    void OnClick() {
+
+        if (player.GetComponent<UnitStats>().skillPointsValue > 0) {
+            if (gameObject.name == "+ButtonStrength") {
+                float value = 1;
+                player.GetComponent<UnitStats>().Strength.BaseValue += value;
+                player.GetComponent<UnitStats>().skillPointsValue -= value;
+                audioSource.PlayOneShot(sounds[Random.Range(0, sounds.Length)]);
+            }
+            if (gameObject.name == "+ButtonAgility") {
+                float value = 1;
+                player.GetComponent<UnitStats>().Agility.BaseValue += value;
+                player.GetComponent<UnitStats>().skillPointsValue -= value;
+                audioSource.PlayOneShot(sounds[Random.Range(0, sounds.Length)]);
+            }
+            if (gameObject.name == "+ButtonIntelligence") {
+                float value = 1;
+                player.GetComponent<UnitStats>().Intelligence.BaseValue += value;
+                player.GetComponent<UnitStats>().skillPointsValue -= value;
+                audioSource.PlayOneShot(sounds[Random.Range(0, sounds.Length)]);
+            }
+            if (gameObject.name == "+ButtonSpirit") {
+                float value = 1;
+                player.GetComponent<UnitStats>().Spirit.BaseValue += value;
+                player.GetComponent<UnitStats>().skillPointsValue -= value;
+                audioSource.PlayOneShot(sounds[Random.Range(0, sounds.Length)]);
+            }
+            if (gameObject.name == "+ButtonVitality") {
+                float value = 1;
+                player.GetComponent<UnitStats>().Vitality.BaseValue += value;
+                player.GetComponent<UnitStats>().skillPointsValue -= value;
+                audioSource.PlayOneShot(sounds[Random.Range(0, sounds.Length)]);
+            }
+        }
+    }
+}
---
+++ GoKillRaidBoss/Assets/ApprovedAssets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static GameObject instance;
+
+    //UI Элементы
+    public GameObject healthBarEnemy;
+    public GameObject menuCharacter;
+
+    void Awake () {
+        instance = this.gameObject; //Ссылка на обьект GameSettings статическая
+    }
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.I)) {
+            if (menuCharacter.activeSelf) {
+                menuCharacter.SetActive(false);
+            } else {
+                menuCharacter.SetActive(true);
+            }
+        }
+    }
+}
